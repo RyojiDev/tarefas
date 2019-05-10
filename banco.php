a597c594cc84c9406a33489dbdbecc64389f9a3e
ryoji <ryojikitanodev@gmail.com>
2019-05-10 17:04:10 -0300

    first commit

---
+++ banco.php
@@ -0,0 +1,19 @@
+<?php
+
+$bdServidor = '127.0.0.1';
+$bdUsuario = 'sistematarefa';
+$bdSenha = 'sistema';
+$bdBanco = 'tarefas';
+
+$conexao = mysqli_connect($bdServidor, $bdUsuario , $bdSenha, $bdBanco);
+
+if(mysqli_connect_errno($conexao)){
+	echo "Problemas para conectar no banco verifique os dados!";
+	die();
+}
+
+?>
+
+
+
+
